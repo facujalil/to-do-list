b1414186f7b28a61ca5e6352e1292544bd947af1
Facu J <facu.jalil@gmail.com>
2024-09-15 23:52:29 -0300

    rename variables and classes for current date

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -4,22 +4,22 @@ import "../css/CurrentDate.css";
 function CurrentDate() {
   const date = new Date();
 
-  const dd = date.getDate();
-  const mm = date.toLocaleString("es-AR", { month: "short" });
-  const yyyy = date.getFullYear();
-  const day = date.toLocaleString("es-AR", { weekday: "long" });
+  const day = date.getDate();
+  const month = date.toLocaleString("es-AR", { month: "short" });
+  const year = date.getFullYear();
+  const weekday = date.toLocaleString("es-AR", { weekday: "long" });
 
   return (
     <div className="current-date">
-      <div className="dd-mm-yyyy">
-        <span className="dd">{dd}</span>
-        <div className="mm-yyyy">
-          <span className="mm">{mm}</span>
-          <span className="yyyy">{yyyy}</span>
+      <div className="day-month-year">
+        <span className="day">{day}</span>
+        <div className="month-year">
+          <span className="month">{month}</span>
+          <span className="year">{year}</span>
         </div>
       </div>
-      <div className="day">
-        {day.split("").map((letter, index) => (
+      <div className="weekday">
+        {weekday.split("").map((letter, index) => (
           <span key={index}>{letter}</span>
         ))}
       </div>
